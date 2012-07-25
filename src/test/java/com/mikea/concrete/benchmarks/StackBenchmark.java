74865f1d56834acdc803c3d42246a7d237134f5a
Mike Aizatsky <mike.aizatsky@gmail.com>
2012-07-24 21:18:14 -0700

    Reworked stack benchmarks.

--- src/test/java/com/mikea/concrete/benchmarks/StackBenchmark.java
+++ src/test/java/com/mikea/concrete/benchmarks/StackBenchmark.java
@@ -7,6 +7,8 @@ import com.mikea.concrete.Stack;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Lists.newLinkedList;
@@ -21,10 +23,52 @@ public class StackBenchmark extends SimpleBenchmark {
 
   @Param
   private Implementation implementation;
+  private List<Op> ops;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    ops = test.generateOps(length);
+  }
 
   public void time(int reps) {
+    Object o = "foo";
     for (int i = 0; i < reps; ++i) {
-      test.rep(implementation, length);
+      ArrayList<Object> arrayList = newArrayList();
+      LinkedList<Object> linkedList = newLinkedList();
+      Stack<Object> stack = newStack();
+
+      for (Op op : ops) {
+        switch (op) {
+          case PUSH_FRONT:
+            switch (implementation) {
+              case LINKED_LIST:
+                linkedList.addFirst(o);
+                break;
+              case PSTACK:
+                stack = stack.pushFront(o);
+                break;
+              case ARRAY_LIST:
+                arrayList.add(o);
+                break;
+            }
+            break;
+          case POP_FRONT:
+            switch (implementation) {
+              case LINKED_LIST:
+                linkedList.removeFirst();
+                break;
+              case PSTACK:
+                stack = stack.popFront();
+                break;
+              case ARRAY_LIST:
+                arrayList.remove(arrayList.size() - 1);
+                break;
+            }
+            break;
+        }
+      }
     }
   }
 
@@ -32,92 +76,85 @@ public class StackBenchmark extends SimpleBenchmark {
     LINKED_LIST, PSTACK, ARRAY_LIST
   }
 
+  enum Op {
+    PUSH_FRONT, POP_FRONT
+  }
+
   enum Test {
     PUSH_FRONT {
       @Override
-      void rep(Implementation implementation, int length) {
-        switch (implementation) {
-          default:
-            throw new IllegalArgumentException();
-
-          case LINKED_LIST: {
-            LinkedList<String> stack = newLinkedList();
-            String object = "foo";
-            for (int j = 0; j < length; ++j) {
-              stack.add(object);
-            }
-            break;
-          }
-
-          case PSTACK: {
-            Stack<String> stack = newStack();
-            String object = "foo";
-            for (int j = 0; j < length; ++j) {
-              stack = stack.pushFront(object);
-            }
-            break;
-          }
-
-          case ARRAY_LIST: {
-            ArrayList<String> stack = newArrayList();
-            String object = "foo";
-            for (int j = 0; j < length; ++j) {
-              stack.add(object);
-            }
-            break;
-          }
-
+      List<Op> generateOps(int length) {
+        List<Op> result = newArrayList();
+        for (int i = 0; i < length; ++i) {
+          result.add(Op.PUSH_FRONT);
         }
+        return result;
       }
     },
+
     PUSH_ALL_POP_ALL {
       @Override
-      void rep(Implementation implementation, int length) {
-        switch (implementation) {
-          default:
-            throw new IllegalArgumentException();
-
-          case LINKED_LIST: {
-            LinkedList<String> stack = newLinkedList();
-            String object = "foo";
-            for (int j = 0; j < length; ++j) {
-              stack.addFirst(object);
-            }
-            for (int j = 0; j < length; ++j) {
-              stack.pop();
-            }
-            break;
-          }
-
-          case PSTACK: {
-            Stack<String> stack = newStack();
-            String object = "foo";
-            for (int j = 0; j < length; ++j) {
-              stack = stack.pushFront(object);
-            }
-            for (int j = 0; j < length; ++j) {
-              stack = stack.popFront();
-            }
-            break;
-          }
-
-          case ARRAY_LIST: {
-            ArrayList<String> stack = newArrayList();
-            String object = "foo";
-            for (int j = 0; j < length; ++j) {
-              stack.add(object);
-            }
-            for (int j = 0; j < length; ++j) {
-              stack.remove(stack.size() - 1);
-            }
-            break;
-          }
-
+      List<Op> generateOps(int length) {
+        List<Op> result = newArrayList();
+        for (int i = 0; i < length; ++i) {
+          result.add(Op.PUSH_FRONT);
         }
+        for (int i = 0; i < length; ++i) {
+          result.add(Op.POP_FRONT);
+        }
+        return result;
+      }
+    },
+
+    RANDOM {
+      @Override
+      public List<Op> generateOps(int length) {
+        Random rnd = new Random();
+        List<Op> result = newArrayList();
+
+        int sz = 0;
+
+        while (result.size() < length) {
+          switch (rnd.nextInt(2)) {
+            case 0: {
+              sz++;
+              result.add(Op.PUSH_FRONT);
+              break;
+            }
+
+            case 1: {
+              if (sz > 0) {
+                result.add(Op.POP_FRONT);
+                sz--;
+              }
+              break;
+            }
+          }
+        }
+
+        return result;
+      }
+    },
+
+    ZIGZAG {
+      @Override
+      List<Op> generateOps(int length) {
+        List<Op> result = newArrayList();
+
+        for (int i = 0; i < length; ++i) {
+          for (int j = i; j < length; ++ j) {
+            result.add(Op.PUSH_FRONT);
+          }
+          for (int j = i; j < length - 1; ++ j) {
+            result.add(Op.POP_FRONT);
+          }
+        }
+
+        return result;
       }
     };
 
-    abstract void rep(Implementation implementation, int length);
+    abstract List<Op> generateOps(int length);
   }
 
   public static void main(String[] args) {
